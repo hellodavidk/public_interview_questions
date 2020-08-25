62d7cb4c0bfba083fb7acc59016504bf081fc08c
David Kim <hellodavidskim@gmail.com>
2020-08-24 23:40:27 -0700

    Whoops

--- solutions/three_sum_solution.py
+++ solutions/three_sum_solution.py
@@ -23,28 +23,4 @@ from typing import List
 
 class Solution:
     def threeSum(self: Solution, nums: List[int]) -> List[List[int]]:
-        answer = []
-        nums.sort()
-        for i in range(len(nums) - 2):
-            if nums[i] > 0:
-                break
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            low = i + 1
-            high = len(nums) - 1
-            while low < high:
-                current = nums[low] + nums[high] + nums[i]
-                if current < 0:
-                    low += 1
-                elif current > 0:
-                    high -= 1
-                else:
-                    answer.append([nums[i], nums[low], nums[high]])
-                    while low < high and nums[low] == nums[low + 1]:
-                        low += 1
-                    while low < high and nums[high] == nums[high - 1]:
-                        high -= 1
-                    low += 1
-                    high -= 1
-
-        return answer
+        pass
